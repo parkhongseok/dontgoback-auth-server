06bb979bd101b07fddcf00a33674a4dfa8eb39b2
parkhongseok <123qkrghdtjr@naver.com>
2025-08-03 17:09:38 +0900

    init: new project

---
+++ dg-auth-server/src/main/java/com/dontgoback/msa/auth/DgAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.dontgoback.msa.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgAuthServerApplication.class, args);
+	}
+
+}
---
+++ dg-auth-server/src/test/java/com/dontgoback/msa/auth/DgAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dontgoback.msa.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
